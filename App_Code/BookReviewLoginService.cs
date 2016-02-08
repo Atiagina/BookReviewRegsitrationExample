dc116b69baa6e8dbb32794a5256644a4519e2bb8
Anna <atiagina@gmail.com>
2016-02-08 11:23:19 -0800

    Adds Example of Reviewer's Registration service

---
+++ App_Code/BookReviewLoginService.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+
+// NOTE: You can use the "Rename" command on the "Refactor" menu to change the class name "BookReviewLoginService" in code, svc and config file together.
+public class BookReviewLoginService : IBookReviewLoginService
+{
+    BookReviewDbEntities db = new BookReviewDbEntities();
+
+    
+
+    public int ReviewerLogin(string username, string password)
+    {
+         int result = db.usp_ReviewerLogin(username, password);
+        if (result != -1)
+        {
+            var key = from k in db.Reviewers
+                      where k.ReviewerUserName.Equals(username)
+                      select new { k.ReviewerKey };
+
+            foreach (var k in key){
+
+            result = (int)k.ReviewerKey;
+        }
+        }
+        
+        return result;
+    }
+
+    public int ReviewerRegistration(ReviewerLite r)
+    {
+        Reviewer rv = new Reviewer();
+
+
+        int result = db.usp_NewReviewer(r.UserName, r.FirstName, r.LastName, r.Email, r.Password);
+        return result;
+    }
+}
---
+++ App_Code/IBookReviewLoginService.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+
+// NOTE: You can use the "Rename" command on the "Refactor" menu to change the interface name "IBookReviewLoginService" in both code and config file together.
+[ServiceContract]
+public interface IBookReviewLoginService
+{
+    [OperationContract]
+
+    int ReviewerLogin(string username, string password);
+
+    [OperationContract]
+    int ReviewerRegistration(ReviewerLite r);
+}
+
+[DataContract]
+public class ReviewerLite
+{
+
+    [DataMember]
+    public string LastName { set; get; }
+
+    [DataMember]
+    public string FirstName { set; get; }
+
+    [DataMember]
+    public string UserName { set; get; }
+
+    [DataMember]
+    public string Password { set; get; }
+
+    [DataMember]
+    public string Email { set; get; }
+}
